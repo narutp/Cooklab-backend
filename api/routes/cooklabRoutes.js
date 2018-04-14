196271fe34234e4439722d85b037ea074f472991
suppok <crystalpok@gmail.com>
2018-04-14 15:05:32 +0700

    Refactor in Route

--- api/routes/cooklabRoutes.js
+++ api/routes/cooklabRoutes.js
@@ -1,47 +1,47 @@
 'use strict';
 
 module.exports = function(app) {
-  var cooklab = require('../controllers/cooklabController');
-  var posting = require('../controllers/postingController');
-  var user = require('../controllers/userController');
-  var dish = require('../controllers/dishController');
+  var cooklab = require('../controllers/cooklabController')
+  var posting = require('../controllers/postingController')
+  var user = require('../controllers/userController')
+  var dish = require('../controllers/dishController')
 
 
   app.route('/achievements')
     .get(cooklab.list_all_achievements)
 
   app.route('/create_achievement')
-    .post(cooklab.create_new_achievement);
+    .post(cooklab.create_new_achievement)
 
   app.route('/comments')
-    .get(posting.list_all_comments);
+    .get(posting.list_all_comments)
 
   app.route('/create_comment')
-    .post(posting.create_new_comment);
+    .post(posting.create_new_comment)
 
   app.route('/dishes')
-    .get(dish.list_all_dishes);
+    .get(dish.list_all_dishes)
 
   app.route('/create_dish')
-    .post(dish.create_new_dish);
+    .post(dish.create_new_dish)
 
   app.route('/ingredients')
-    .get(dish.list_all_ingredients);
+    .get(dish.list_all_ingredients)
 
   app.route('/create_ingredient')
-    .post(dish.create_new_ingredient);
+    .post(dish.create_new_ingredient)
 
   app.route('/posts')
-    .get(posting.list_all_posts);
+    .get(posting.list_all_posts)
 
   app.route('/create_post')
-    .post(posting.create_new_post);
+    .post(posting.create_new_post)
 
   app.route('/users')
     .get(user.list_all_users)
 
   app.route('/create_user')
-    .post(user.create_new_user);
+    .post(user.create_new_user)
 
   app.route('/get_achievement')
     .get(cooklab.get_achievement)
@@ -50,7 +50,7 @@ module.exports = function(app) {
     .put(cooklab.update_achievement)
 
   app.route('/delete_achievement')
-    .delete(cooklab.delete_achievement);
+    .delete(cooklab.delete_achievement)
 
   app.route('/get_comment')
     .get(posting.get_comment)
@@ -59,7 +59,7 @@ module.exports = function(app) {
     .put(posting.update_comment)
 
   app.route('/delete_comment')
-    .delete(posting.delete_comment);
+    .delete(posting.delete_comment)
 
   app.route('/get_dish')
     .get(dish.get_dish)
@@ -68,7 +68,7 @@ module.exports = function(app) {
     .put(dish.update_dish)
 
   app.route('delete_dish')
-    .delete(dish.delete_dish); 
+    .delete(dish.delete_dish)
     
   app.route('/update_ingredient')
     .put(dish.update_ingredient)
@@ -86,50 +86,47 @@ module.exports = function(app) {
     .put(posting.update_post)
 
   app.route('/delete_post')
-    .delete(posting.delete_post);
+    .delete(posting.delete_post)
 
   app.route('/posts/userId/:userId')
-    .get(posting.get_images_posts_by_user_id);
+    .get(posting.get_images_posts_by_user_id)
 
   app.route('/users')
-    .delete(user.delete_user);
+    .delete(user.delete_user)
 
   app.route('/update_user')
-    .put(user.update_user);
+    .put(user.update_user)
 
   app.route('/get_user')
     .get(user.get_user)
 
   app.route('/follow')
-    .put(user.follow_user);
+    .put(user.follow_user)
 
   app.route('/unfollow')
-    .put(user.unfollow_user);
+    .put(user.unfollow_user)
 
   app.route('/feeds')
-    .get(posting.get_feeds_by_user_id);
+    .get(posting.get_feeds_by_user_id)
 
   app.route('/topfeed')
-    .get(posting.get_top_feed);
+    .get(posting.get_top_feed)
 
   app.route('/increase_trophy')
-    .put(posting.increase_trophy);
+    .put(posting.increase_trophy)
     
   app.route('/decrease_trophy')
-    .put(posting.decrease_trophy);
+    .put(posting.decrease_trophy)
 
   app.route('/login')
-    .post(user.login_by_username_and_password);
+    .post(user.login_by_username_and_password)
 
   app.route('/delete_all_user')
-    .delete(user.delete_all_user);
+    .delete(user.delete_all_user)
 
   app.route('/delete_all_post')
-    .delete(posting.delete_all_post);
+    .delete(posting.delete_all_post)
 
   app.route('/get_user_id')
-    .get(user.get_id_user_by_username);
-
-  app.route('/search_user')
-
+    .get(user.get_id_user_by_username)
 };
